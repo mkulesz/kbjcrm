d3cd8052971a3c878d15b83aa066933e9b0930f9
mkulesz <michal.kulesz@gmail.com>
2017-04-21 14:13:47 +0200

    Change to local timezone

--- kbjcrm_loc/settings.py
+++ kbjcrm_loc/settings.py
@@ -105,15 +105,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
+from django.utils import timezone
+
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = timezone.now()
 
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
+#USE_I18N = True
+#USE_L10N = True
+#USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
