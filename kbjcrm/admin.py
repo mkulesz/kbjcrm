06576041008286d6fadcd1f9fdbde763575fe880
mkulesz <michal.kulesz@gmail.com>
2017-04-21 10:39:36 +0200

    First to GO :)

--- kbjcrm/admin.py
+++ kbjcrm/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from.models import OsobaKontaktowa
+#from.models import OsobaKontaktowa
+from.models import Osoba
 # Register your models here.
 
-admin.site.register(OsobaKontaktowa)
+#admin.site.register(OsobaKontaktowa)
+admin.site.register(Osoba)
--- kbjcrm/models.py
+++ kbjcrm/models.py
@@ -1,12 +1,29 @@
 from django.db import models
 
 # Create your models here.
-
+'''
 class OsobaKontaktowa(models.Model):
     pesel       = models.CharField(max_length=11, primary_key=True, )
     imie        = models.CharField(max_length=15)
     nazwisko    = models.CharField(max_length=20)
 
+    class Meta:
+        verbose_name_plural = "Osoba Kontaktowa"
+
     def __str__(self):
         rv_value = self.pesel + self.imie + self.nazwisko
         return rv_value
+'''
+class Osoba(models.Model):
+    IdOsoba     = models.IntegerField(auto_created=True, primary_key=True)
+    Imie        = models.CharField(max_length=20)
+    Nazwisko    = models.CharField(max_length=40)
+    Telefon     = models.IntegerField()
+    Mail        = models.EmailField()
+    Login       = models.CharField(max_length=10)
+    Hasło       = models.CharField(max_length=20)
+    class Meta:
+        verbose_name_plural = "Osoba"
+
+
+
