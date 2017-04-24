1003a8cf39fbf085d601112edc645840bef15087
mkulesz <michal.kulesz@gmail.com>
2017-04-24 15:22:11 +0200

    Works? Tests Tests

--- kbjcrm/views.py
+++ kbjcrm/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from kbjcrm.control import Control
+from django.forms import Form
 
 # Create your views here.
 from django.http import HttpResponse
@@ -8,6 +9,9 @@ def views(request):
     return render(request, 'views/first_view.html', {})
 
 def rcp(request):
+    if request.method == 'POST':
+        year = request.POST.get('Year')
 #    year = request.POST['Year']
-    lv_list = Control.Select(2017)
-    return render(request, 'views/rcp_view.html', {'obj': lv_list})
+        lv_list = Control.Select(year)
+        return render(request, 'views/rcp_view.html', {'obj': lv_list})
+    return render(request, 'views/rcp_view.html', {'obj': ''})
