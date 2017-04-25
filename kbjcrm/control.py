a2698c19bf135ca9eb703dc2036a28119e8be0b3
mkulesz <michal.kulesz@gmail.com>
2017-04-25 12:58:46 +0200

    new stuff + refactoring

--- kbjcrm/control.py
+++ kbjcrm/control.py
@@ -1,11 +1,21 @@
 from kbjcrm.models import *
+from datetime import *
 
 class Control:
     def select( year ):
         select = Naleznosc.objects.filter(Data_platnosci__year = year)
         return select
 
-    def generate( number ):
+    def generate( number, idrcp ):
+        rcp = Naleznosc.objects.filter(IDKonta=idrcp)
+        RCP = Naleznosc()
+        RCP.IDKonta = idrcp
+        StartDate = rcp.Data_platnosci
+        RCP.Data_platnosci = StartDate.replace(StartDate.year + 1)
+        RCP.Tytul = rcp.Tytul + ' ' + str(RCP.Data_platnosci)
+        RCP.Kwota_oplaty = rcp.Kwota_oplaty
+        RCP.Zrealizowane = rcp.Zrealizowane
+        RCP.save()
 
         return 0
 
--- kbjcrm/views.py
+++ kbjcrm/views.py
@@ -5,7 +5,10 @@ from kbjcrm.control import Control
 
 def rcp(request):
     if request.method == 'POST':
-        year = request.POST.get('Year')
-        lv_list = Control.select(year)
-        return render(request, 'views/rcp_view.html', {'obj': lv_list, 'rcplist': lv_list})
+        if 'List' in request.POST:
+            year = request.POST.get('Year')
+            return render(request, 'views/rcp_view.html', {'obj': Control.select(year), 'rcplist': Control.select(year)})
+        if 'Generator' in request.POST:
+            number = request.POST.get('Generator_num')
+            return render(request, 'views/rcp_view.html', {'obj': '', 'rcplist': Control.select_all(Control), 'gen_number': number})
     return render(request, 'views/rcp_view.html', {'obj': '', 'rcplist': Control.select_all(Control)})
