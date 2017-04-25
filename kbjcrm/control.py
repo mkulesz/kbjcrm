49e21e022c28dd3bca2c676d20e9f3ed55fadd2e
mkulesz <michal.kulesz@gmail.com>
2017-04-25 10:23:14 +0200

    Add generator

--- kbjcrm/control.py
+++ kbjcrm/control.py
@@ -1,6 +1,14 @@
 from kbjcrm.models import *
 
 class Control:
-    def Select( year ):
+    def select( year ):
         select = Naleznosc.objects.filter(Data_platnosci__year = year)
         return select
+
+    def generate( number ):
+
+        return 0
+
+    def select_all(_self_):
+        select_all = Naleznosc.objects.all()
+        return select_all
--- kbjcrm/views.py
+++ kbjcrm/views.py
@@ -6,6 +6,6 @@ from kbjcrm.control import Control
 def rcp(request):
     if request.method == 'POST':
         year = request.POST.get('Year')
-        lv_list = Control.Select(year)
-        return render(request, 'views/rcp_view.html', {'obj': lv_list})
-    return render(request, 'views/rcp_view.html', {'obj': ''})
+        lv_list = Control.select(year)
+        return render(request, 'views/rcp_view.html', {'obj': lv_list, 'rcplist': lv_list})
+    return render(request, 'views/rcp_view.html', {'obj': '', 'rcplist': Control.select_all(Control)})
